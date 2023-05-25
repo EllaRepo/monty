44fcf571b7f47a0ccdf4c7bb2dfb5b8893cf2c75
Eliab E <eliaberango@gmail.com>
2023-05-25 16:09:51 -0400

    Add swap

--- monty.h
+++ monty.h
@@ -73,6 +73,7 @@ void _push(stack_t **, unsigned int);
 void _pall(stack_t **, unsigned int);
 void _pint(stack_t **, unsigned int);
 void _pop(stack_t **, unsigned int);
+void _swap(stack_t **, unsigned int);
 void (*get_func(char *opcode))(stack_t **stack, unsigned int line_number);
 /* Doubly linked list*/
 stack_t *add_dnodeint(stack_t **head, const int n);
